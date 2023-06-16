09b2bc9bb7dc691c1e109df9f488d1cf8b600de2
jacobo <jacobo.amador@ahinko.com>
2023-06-16 01:15:34 -0600

    bestsellers section

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 import FeaturedSection from '@/components/FeaturedSection'
+import { Arimo } from 'next/font/google'
+
+const arimo = Arimo({
+  subsets: ['latin'],
+  weight: ['700', '400'],
+  variable: '--font-inter',
+})
 
 export default function LandingPage() {
   return (
     <div className="container ">
       <FeaturedSection />
+      <div className="divider-text">
+        <span className={`${arimo.className} font-sans `}>
+          Our best sellers
+        </span>
+      </div>
     </div>
   )
 }
