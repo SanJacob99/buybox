61a4f786c223505af46e52b2042c1fe2972adfc7
jacobo <jacobo.amador@ahinko.com>
2023-06-27 22:47:00 -0600

    feature: category section header

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const categories = () => {
-  return <div>Hello There</div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-8 text left">
+          <div className="col-4">back arrow</div>
+          <div className="col-4">Category</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default categories
